5a7ab57bc539263761dd3a97fa505144c85f3405
giampaolli <felipegiampaoli@yahoo.com.br>
2017-07-21 13:00:27 -0300

    integrando componente com redux

--- ex/field.jsx
+++ ex/field.jsx
@@ -1,25 +1,28 @@
 import React, {Component} from 'react'
+import { connect}  from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { changeValue} from './fieldActions'
 
 class Field extends Component{
-
-    constructor(props){
-        super(props)
-        this.handleChange = this.handleChange.bind(this)
-        this.state = {value: props.inicialValue}
-    }
-
-    handleChange(event){
-        this.setState({value: event.target.value})
-    }
 
     render(){
         return(
             <div>
-                <label>{this.state.value}</label><br/>
-                <input onChange={this.handleChange} value={this.state.value} />
+                <label>{this.props.value}</label><br/>
+                <input onChange={this.props.changeValue} value={this.props.value} />
             </div>
         )
     }
 }
 
-export default Field
+function mapStateToProps(state){
+    return {
+        value: state.field.value
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+     return bindActionCreators({ changeValue }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Field)
